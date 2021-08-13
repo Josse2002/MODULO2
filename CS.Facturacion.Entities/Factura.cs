956ef0e8794103a17a3652eec90ba44668ae9ede
Josse2002 <75065069+Josse2002@users.noreply.github.com>
2021-08-13 12:13:16 -0600

    Add files via upload

---
+++ CS.Facturacion.Entities/Factura.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.Facturacion.Entities
+{
+    public class Factura
+    {
+        public Int64 Id { get; set; }
+
+        public Int64 Numero { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public Int64 IdCliente { get; set; }
+
+        public Double SubTotal { get; set; }
+
+        public Double TotalImpuesto { get; set; }
+
+        public Double Total { get; set; }
+
+        public string Estado { get; set; }
+
+        public Factura() { }
+
+        public Factura(Int64 pId, Int64 pNumero, DateTime pFecha, Int64 pIdCliente, Double pSubTotal, Double pTotalImpuesto,
+            Double pTotal, string pEstado)
+        {
+            Id = pId;
+            Numero = pNumero;
+            Fecha = pFecha;
+            IdCliente = pIdCliente;
+            SubTotal = pSubTotal;
+            TotalImpuesto = pTotalImpuesto;
+            Total = pTotal;
+            Estado = pEstado;
+
+        }
+    }
+}
